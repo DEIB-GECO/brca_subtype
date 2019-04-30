dacd46357a3d4c6008ba81bbc26deb2293f0f2c1
FranciscoCristovao <francisco.m.cristovao@gmail.com>
2019-04-30 17:44:11 +0200

    Added results of organized sweep

--- src/evaluate_vae_tcga.py
+++ src/evaluate_vae_tcga.py
@@ -62,7 +62,7 @@ else:
 	learning_rate = args.learning_rate
 	dropout_input = args.dropout_input
 	dropout_hidden = args.dropout_hidden
-	dropout_decoder = args.dropout_decoder
+	dropout_decoder = True
 	freeze_weights = False
 	classifier_use_z = False
 	reconstruction_loss = args.reconstruction_loss
@@ -99,46 +99,48 @@ validation_set_percent = 0.1
 d_rates = [0, 0.2, 0.4, 0.6, 0.8]
 
 for drop in d_rates:
-	print("DROPOUT RATE FOR ALL LAYERS: {}".format(drop))
-	dropout_input = drop
-	dropout_hidden = drop
-	skf = StratifiedKFold(n_splits=5)
-	scores = []
-	i=1
-	classify_df = pd.DataFrame(columns=["Fold", "accuracy"])
+	for drop2 in d_rates:
+		print("DROPOUT RATE FOR INPUT LAYER: {}".format(drop))
+		print("DROPOUT RATE FOR HIDDEN LAYERS: {}".format(drop2))
+		dropout_input = drop
+		dropout_hidden = drop2
+		skf = StratifiedKFold(n_splits=5)
+		scores = []
+		i=1
+		classify_df = pd.DataFrame(columns=["Fold", "accuracy"])
 
-	for train_index, test_index in skf.split(X_brca_train, y_brca_train):
-		print('Fold {} of {}'.format(i, skf.n_splits))
+		for train_index, test_index in skf.split(X_brca_train, y_brca_train):
+			print('Fold {} of {}'.format(i, skf.n_splits))
 
-		X_train, X_val = X_brca_train.iloc[train_index], X_brca_train.iloc[test_index]
-		y_train, y_val = y_brca_train.iloc[train_index], y_brca_train.iloc[test_index]
+			X_train, X_val = X_brca_train.iloc[train_index], X_brca_train.iloc[test_index]
+			y_train, y_val = y_brca_train.iloc[train_index], y_brca_train.iloc[test_index]
 
-		# Prepare data to train Variational Autoencoder (merge dataframes and normalize)
-		X_autoencoder = pd.concat([X_train, X_tcga_no_brca], sort=True)
-		scaler = MinMaxScaler()
-		scaler.fit(X_autoencoder)
-		X_autoencoder_scaled = pd.DataFrame(scaler.transform(X_autoencoder), columns=X_autoencoder.columns)
+			# Prepare data to train Variational Autoencoder (merge dataframes and normalize)
+			X_autoencoder = pd.concat([X_train, X_tcga_no_brca], sort=True)
+			scaler = MinMaxScaler()
+			scaler.fit(X_autoencoder)
+			X_autoencoder_scaled = pd.DataFrame(scaler.transform(X_autoencoder), columns=X_autoencoder.columns)
 
-		# Scale logistic regression data
-		scaler.fit(X_train)
-		X_train = pd.DataFrame(scaler.transform(X_train), columns=X_train.columns)
-		X_val = pd.DataFrame(scaler.transform(X_val), columns=X_val.columns)
+			# Scale logistic regression data
+			scaler.fit(X_train)
+			X_train = pd.DataFrame(scaler.transform(X_train), columns=X_train.columns)
+			X_val = pd.DataFrame(scaler.transform(X_val), columns=X_val.columns)
 
-		#Split validation set
-		X_autoencoder_val = X_autoencoder_scaled.sample(frac=validation_set_percent)
-		X_autoencoder_train = X_autoencoder_scaled.drop(X_autoencoder_val.index)
-
-
-		# Order the features correctly before training
-
-		X_autoencoder_train = X_autoencoder_train.reindex(sorted(X_autoencoder_train.columns), axis="columns")
-		X_autoencoder_val = X_autoencoder_val.reindex(sorted(X_autoencoder_val.columns), axis="columns")
-		X_train = X_train.reindex(sorted(X_train.columns), axis="columns")
-		X_val = X_val.reindex(sorted(X_val.columns), axis="columns")
+			#Split validation set
+			X_autoencoder_val = X_autoencoder_scaled.sample(frac=validation_set_percent)
+			X_autoencoder_train = X_autoencoder_scaled.drop(X_autoencoder_val.index)
 
 
-		#Train the Model
-		vae = VAE(original_dim=X_autoencoder_train.shape[1], 
+			# Order the features correctly before training
+
+			X_autoencoder_train = X_autoencoder_train.reindex(sorted(X_autoencoder_train.columns), axis="columns")
+			X_autoencoder_val = X_autoencoder_val.reindex(sorted(X_autoencoder_val.columns), axis="columns")
+			X_train = X_train.reindex(sorted(X_train.columns), axis="columns")
+			X_val = X_val.reindex(sorted(X_val.columns), axis="columns")
+
+
+			#Train the Model
+			vae = VAE(original_dim=X_autoencoder_train.shape[1], 
 					intermediate_dim=hidden_dim, 
 					latent_dim=latent_dim, 
 					epochs=epochs, 
@@ -150,20 +152,20 @@ for drop in d_rates:
 					classifier_use_z=classifier_use_z,
 					rec_loss=reconstruction_loss)
 
-		vae.initialize_model()
-		vae.train_vae(train_df=X_autoencoder_train, val_df=X_autoencoder_val)
+			vae.initialize_model()
+			vae.train_vae(train_df=X_autoencoder_train, val_df=X_autoencoder_val)
 
-		# Build and train stacked classifier
-		enc = OneHotEncoder(sparse=False)
-		y_labels_train = enc.fit_transform(y_train.values.reshape(-1, 1))
-		y_labels_val = pd.DataFrame(enc.fit_transform(y_val.values.reshape(-1, 1)))
+			# Build and train stacked classifier
+			enc = OneHotEncoder(sparse=False)
+			y_labels_train = enc.fit_transform(y_train.values.reshape(-1, 1))
+			y_labels_val = pd.DataFrame(enc.fit_transform(y_val.values.reshape(-1, 1)))
 	    
-		X_train_train, X_train_val, y_labels_train_train, y_labels_train_val = train_test_split(X_train, y_labels_train, test_size=0.2, stratify=y_train, random_state=42)
+			X_train_train, X_train_val, y_labels_train_train, y_labels_train_val = train_test_split(X_train, y_labels_train, test_size=0.2, stratify=y_train, random_state=42)
 
-		print("BUILDING CLASSIFIER")
-		vae.build_classifier()
+			print("BUILDING CLASSIFIER")
+			vae.build_classifier()
 
-		fit_hist = vae.classifier.fit(x=X_train_train, 
+			fit_hist = vae.classifier.fit(x=X_train_train, 
 										y=y_labels_train_train, 
 										shuffle=True, 
 										epochs=100,
@@ -171,48 +173,48 @@ for drop in d_rates:
 										callbacks=[EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)],
 										validation_data=(X_train_val, y_labels_train_val))
 
-		if(vae.classifier_use_z):
-			X_val_new = pd.DataFrame(np.repeat(X_val.values, 10, axis=0))
-			X_val_new.columns = X_val.columns
-			y_labels_val_new = pd.DataFrame(np.repeat(y_labels_val.values, 10, axis=0))
+			if(vae.classifier_use_z):
+				X_val_new = pd.DataFrame(np.repeat(X_val.values, 10, axis=0))
+				X_val_new.columns = X_val.columns
+				y_labels_val_new = pd.DataFrame(np.repeat(y_labels_val.values, 10, axis=0))
 
 
-			score = vae.classifier.evaluate(X_val_new, y_labels_val_new)
-		else:
-			score = vae.classifier.evaluate(X_val, y_labels_val)
+				score = vae.classifier.evaluate(X_val_new, y_labels_val_new)
+			else:
+				score = vae.classifier.evaluate(X_val, y_labels_val)
 
-		print(score)
-		scores.append(score[1])
+			print(score)
+			scores.append(score[1])
 
-		classify_df = classify_df.append({"Fold":str(i), "accuracy":score[1]}, ignore_index=True)
-		history_df = pd.DataFrame(fit_hist.history)
+			classify_df = classify_df.append({"Fold":str(i), "accuracy":score[1]}, ignore_index=True)
+			history_df = pd.DataFrame(fit_hist.history)
 
-		filename="../results2/VAE/{}_hidden_{}_emb/history/tcga_classifier_dropout_{}_in_{}_hidden_rec_loss_{}_history_{}_classifier_frozen_{}_cv.csv".format(hidden_dim, latent_dim, dropout_input, dropout_hidden, reconstruction_loss, i, vae.freeze_weights)
-		history_df.to_csv(filename, sep=',')
-		i+=1
+			filename="../results2/VAE/{}_hidden_{}_emb/history/tcga_classifier_dropout_{}_in_{}_hidden_rec_loss_{}_history_{}_classifier_frozen_{}_cv.csv".format(hidden_dim, latent_dim, dropout_input, dropout_hidden, reconstruction_loss, i, vae.freeze_weights)
+			history_df.to_csv(filename, sep=',')
+			i+=1
 
-	print('5-Fold results: {}'.format(scores))
-	print('Average accuracy: {}'.format(np.mean(scores)))
-
-
-	classify_df = classify_df.assign(mean_accuracy=np.mean(scores))
-	classify_df = classify_df.assign(intermediate_dim=hidden_dim)
-	classify_df = classify_df.assign(latent_dim=latent_dim)
-	classify_df = classify_df.assign(batch_size=batch_size)
-	classify_df = classify_df.assign(epochs_vae=epochs)
-	classify_df = classify_df.assign(learning_rate=learning_rate)
-	classify_df = classify_df.assign(dropout_input=dropout_input)
-	classify_df = classify_df.assign(dropout_hidden=dropout_hidden)
-	classify_df = classify_df.assign(dropout_decoder=dropout_decoder)
-	classify_df = classify_df.assign(freeze_weights=freeze_weights)
-	classify_df = classify_df.assign(classifier_use_z=classifier_use_z)
-	classify_df = classify_df.assign(classifier_loss="categorical_crossentropy")
-	classify_df = classify_df.assign(reconstruction_loss=reconstruction_loss)
-
-	output_filename="../results2/VAE/{}_hidden_{}_emb/tcga_classifier_dropout_{}_in_{}_hidden_rec_loss_{}_classifier_frozen_{}_cv.csv".format(hidden_dim, latent_dim, dropout_input, dropout_hidden, reconstruction_loss, freeze_weights)
+		print('5-Fold results: {}'.format(scores))
+		print('Average accuracy: {}'.format(np.mean(scores)))
 
 
-	classify_df.to_csv(output_filename, sep=',')
+		classify_df = classify_df.assign(mean_accuracy=np.mean(scores))
+		classify_df = classify_df.assign(intermediate_dim=hidden_dim)
+		classify_df = classify_df.assign(latent_dim=latent_dim)
+		classify_df = classify_df.assign(batch_size=batch_size)
+		classify_df = classify_df.assign(epochs_vae=epochs)
+		classify_df = classify_df.assign(learning_rate=learning_rate)
+		classify_df = classify_df.assign(dropout_input=dropout_input)
+		classify_df = classify_df.assign(dropout_hidden=dropout_hidden)
+		classify_df = classify_df.assign(dropout_decoder=dropout_decoder)
+		classify_df = classify_df.assign(freeze_weights=freeze_weights)
+		classify_df = classify_df.assign(classifier_use_z=classifier_use_z)
+		classify_df = classify_df.assign(classifier_loss="categorical_crossentropy")
+		classify_df = classify_df.assign(reconstruction_loss=reconstruction_loss)
+
+		output_filename="../results2/VAE/{}_hidden_{}_emb/tcga_classifier_dropout_{}_in_{}_hidden_rec_loss_{}_classifier_frozen_{}_cv.csv".format(hidden_dim, latent_dim, dropout_input, dropout_hidden, reconstruction_loss, freeze_weights)
+
+
+		classify_df.to_csv(output_filename, sep=',')
 
 '''
 #################################
